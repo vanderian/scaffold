967f8a13466fce9e006158f0a2b8b2ef82b8986a
Marian Vanderka <vanderka.marian@gmail.com>
2018-05-29 00:10:48 +0200

    default request code = 0

--- lib/src/main/java/com/vander/scaffold/screen/DataModels.kt
+++ lib/src/main/java/com/vander/scaffold/screen/DataModels.kt
@@ -20,8 +20,8 @@ class NextScreen(val screen: Screen<*, *>, val id: Int = R.id.container_id, val 
 class NextScreenResult(val screen: Screen<*, *>, val requestCode: Int, val id: Int = R.id.container_id, val fragmentsManager: Boolean = false) : Navigation()
 class NextActivity(val intent: Intent, val finish: Boolean = false) : Navigation()
 class NextActivityExplicit(val clazz: KClass<out Activity>, val finish: Boolean = false) : Navigation()
-class WithResult(val intent: Intent, val requestCode: Int) : Navigation()
-class WithResultExplicit(val clazz: KClass<out Activity>, val requestCode: Int) : Navigation()
+class WithResult(val intent: Intent, val requestCode: Int = 0) : Navigation()
+class WithResultExplicit(val clazz: KClass<out Activity>, val requestCode: Int = 0g) : Navigation()
 
 data class ToastEvent(val msgRes: Int = -1, val msg: String = "", val length: Int = Toast.LENGTH_SHORT) : Event
 interface Notification : Event
